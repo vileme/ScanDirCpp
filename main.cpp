5a4cdfb2bcf561b3804832b668e591d4a5ec3251
vileme <45894466+vileme@users.noreply.github.com>
2018-12-15 13:33:53 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ int main(int argc, char *argv[])
     QCoreApplication::setLibraryPaths(paths);
     QApplication a(argc, argv);
     main_window w;
+    w.setMinimumHeight(768);
+    w.setMinimumWidth(1024);
     w.show();
 
     return a.exec();
